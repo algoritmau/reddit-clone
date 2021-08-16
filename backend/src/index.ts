f50b5104fcbd6537689886454258b571e0fd5948
Mauricio Paternina <spaceinvadev@gmail.com>
2021-08-16 12:03:23 -0500

    set up apollo-server and express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
 import mikroOrmConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
   await orm.getMigrator().up()
 
-  const post = orm.em.create(Post, { title: 'My first Post' })
+  const app = express()
 
-  // const posts = await orm.em.find(Post, {})
-  console.log(post)
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  })
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('Listening on 4000...')
+  })
 }
 
 main().catch((err) => console.error(err))
